9d730cab4697f5f23dbcfde461c1b020389ab511
Lucas Menezes <lucasm@lucasm.dev>
2025-07-24 15:30:01 -0300

    Refactor BlurBackground component and update locale files for improved structure and clarity; remove unused Bing Travel SVG and add Bing Maps SVG

--- components/BlurBackground/index.tsx
+++ components/BlurBackground/index.tsx
@@ -1,7 +1,7 @@
 const BlurBackground = () => {
   return (
     <div className="fixed -top-100 inset-0 -z-10 flex items-center justify-center">
-      <div className="w-[90%] h-[600px] rounded-full blur-xl md:blur-3xl opacity-70 bg-linear-[90deg,var(--color-red)_14%,var(--color-orange)_28%,var(--color-yellow)_42%,var(--color-green)_56%,var(--color-blue)_70%,var(--color-purple)_84%,var(--color-pink)_98%] "></div>
+      <div className="w-[90%] h-[50%] rounded-full blur-xl md:blur-2xl opacity-70 bg-linear-[90deg,var(--color-red)_14%,var(--color-orange)_28%,var(--color-yellow)_42%,var(--color-green)_56%,var(--color-blue)_70%,var(--color-purple)_84%,var(--color-pink)_98%] "></div>
     </div>
   )
 }
