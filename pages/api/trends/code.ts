6a35ba1f259ad89877d546ecef43e86ff13cf47a
Lucas Menezes <lucasm@lucasm.dev>
2025-07-24 10:54:12 -0300

    Update API endpoint to fetch top GitHub repositories and adjust response mapping

--- pages/api/trends/code.ts
+++ pages/api/trends/code.ts
@@ -6,25 +6,27 @@ export default async function endpoint(
   _req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const url = 'https://api.gitterapp.com'
+  const url =
+    'https://api.github.com/search/repositories?q=stars:%3E10000&sort=stars&order=desc'
+
+  // https://api.gitterapp.com
 
   try {
     const { data } = await axios.get(url)
 
-    const trends = data
-      .slice(0, 22)
-      .map(
-        (item: {
-          author: string
-          name: string
-          url: string
-          avatar: string
-        }) => ({
-          title: `${item.author}/${item.name}`,
-          url: item.url,
-          image: item.avatar,
-        })
-      )
+    const trends = data.items.slice(0, 22).map(
+      (item: {
+        full_name: string
+        html_url: string
+        owner: {
+          avatar_url: string
+        }
+      }) => ({
+        title: `${item.full_name}`,
+        url: item.html_url,
+        image: item.owner.avatar_url,
+      })
+    )
 
     const responsePayload: ITrends = {
       credits_title: 'GitHub',
