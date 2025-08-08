f55c5449dee87c385d69e52bd5803538e3453277
Lucas Menezes <lucasm@lucasm.dev>
2025-08-08 17:49:06 -0300

    fix: mobile safari on iOS that donts render gradient animations

--- components/BlurBackground/index.tsx
+++ components/BlurBackground/index.tsx
@@ -2,9 +2,9 @@ const BlurBackground = () => {
   return (
     <div
       className={`
-        fixed -top-45 -z-10 moveGradient
+        fixed -top-45 -z-10
 
-        w-[90%] h-[70%] md:h-[90%] rounded-full blur-2xl md:blur-3xl opacity-90
+        w-[90%] h-[70%] md:h-[90%] rounded-full blur-xl md:blur-3xl opacity-90
 
         bg-[linear-gradient(270deg,var(--color-red)_14%,var(--color-orange)_28%,var(--color-yellow)_42%,var(--color-green)_56%,var(--color-blue)_70%,var(--color-purple)_84%,var(--color-pink)_98%)]
         `}></div>
